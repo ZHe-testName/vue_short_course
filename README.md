42e58d5e0cec1dd30e7812045f196cba219fd2e7
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-02-10 16:30:49 +0200

    Create base files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # vue_short_course
-My first try to play with VueJS on UlbiTV course
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- В секции template мы описываем наши шаблоны
+    и всю разметку -->
+    <div>
+        <div>
+            <!-- слушатели собитий привязываются к элементам с помощю дирректив -->
+            <!-- диррективу v-on можно заменить на знак собаки @ это более короткая запись -->
+            <button @click="addLike">Like</button>
+            <button @click="addDislike">Dislike</button>
+        </div>
+
+        <!-- С помощю интерполяции мы можем обращаться к данным из моделей -->
+        <div>Количество лайков : <strong>{{likes}}</strong> </div>
+        <div>Количество дизлайков : <strong>{{dislikes}}</strong> </div>
+    </div>
+</template>
+
+<script>
+// В секции script мы описываем всю логику и остальное
+// по сути это и есть наш компонент
+
+// Секция по дефолту должна возвращать объект в котором и 
+// будут наши модели
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        };
+    },
+    // Функции описываются в поле methods
+    methods: {
+        //для обращения к данным из data используем this
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.dislikes += 1;
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
